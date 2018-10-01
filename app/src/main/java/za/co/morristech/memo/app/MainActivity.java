edc00fa0aec4aa4b65f619566185b66399573589
Wade <wade.fbi@gmail.com>
2018-10-01 13:44:58 +0200

    Added Launcher Icon

--- app/src/main/java/za/co/morristech/memo/app/MainActivity.java
+++ app/src/main/java/za/co/morristech/memo/app/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
